fa91f4cf0b0aaa4d9ae59b0f2416bfee38ecf101
poison-pwn <45178461+poison-pwn@users.noreply.github.com>
2021-08-11 00:07:18 +0530

    renamed Crypto to SymmetricCrypto encrypt and decrypt funcs in SymmetricCrypto return .hex() of the bytes added documentation for Crypto funcs

--- crypto.py
+++ crypto.py
@@ -3,13 +3,19 @@ from nacl.utils import random
 from pyargon2 import hash
 
 
-class Crypto:
+class SymmetricCrypto:
     KDF_SALT_BYTES = 32
 
-    def __gen_secret_box_from_pass(passwd, salt=None):
+    def gen_secret_box_from_pass(passwd, salt=None):
+        """
+        performs a key derivation from given password
+        if salt is not specified a random salt is generated and used
+        to create an NaCl SecretBox instance with the credentials
+        which is returned along with the salt used
+        """
         # perform key derivation from password passed in by user
         if salt is None:
-            salt = random(Crypto.KDF_SALT_BYTES).hex()
+            salt = random(SymmetricCrypto.KDF_SALT_BYTES).hex()
         derived_symmetric_key = hash(
             passwd,
             salt,
@@ -21,9 +27,18 @@ class Crypto:
         return SecretBox(derived_symmetric_key), salt
 
     def encrypt(message, passwd, salt=None):
-        secret_box, salt = Crypto.__gen_secret_box_from_pass(passwd, salt)
-        return secret_box.encrypt(message), salt
+        """
+        encrypt with NaCl secretbox
+        if salt is not provided a random salt is generated
+        returns a tuple of the hex of the encrypted message and the salt used
+        """
+        secret_box, salt = SymmetricCrypto.gen_secret_box_from_pass(passwd, salt)
+        return secret_box.encrypt(message).hex(), salt
 
     def decrypt(cipher, passwd, salt):
-        secret_box, _ = Crypto.__gen_secret_box_from_pass(passwd, salt)
-        return secret_box.decrypt(cipher)
+        """
+        decrypts cipher text with SecretBox
+        returns hex of decryption
+        """
+        secret_box, _ = SymmetricCrypto.gen_secret_box_from_pass(passwd, salt)
+        return secret_box.decrypt(cipher).hex()
