4e826903eb4899e374c54d37f54478b18603b8af
poison-pwn <45178461+poison-pwn@users.noreply.github.com>
2021-08-19 05:23:23 +0530

    added documentation to PostInit metaclass

--- class_utils.py
+++ class_utils.py
@@ -1,4 +1,18 @@
 class PostInit(type):
+    """
+    enables the use of a class as a psuedofunction of sorts
+
+    it overrides the __call__ of `type` class to actuall call an internel function
+    of the class itself
+
+    i.e when the actual class constructor is called
+
+    value = Something() # class which has metaclass PostInit
+
+    a class instance is NOT created, instance it returns the output of the __post_init__
+    of the Something class
+    """
+
     def __call__(cls, *args, **kwargs):
         try:
             return type.__call__(cls, *args, **kwargs).__post_init__()
