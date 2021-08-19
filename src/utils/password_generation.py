390a648872ab82d4e68a776abcd609815e6a46fc
poison-pwn <45178461+poison-pwn@users.noreply.github.com>
2021-08-19 19:38:17 +0530

    made WORD_LEN_RANGE an argument to the xkcd func and added support for it to be an integer also in which case it just checks if the length of the word is equal to that integer
    2. updated the word list file's path relative to the
       password_generation.py file's path
    
    3. updated the line range to be from 4 to 7700
    
    4. added a '__name__ == ' clause at the bottom

--- src/utils/password_generation.py
+++ src/utils/password_generation.py
@@ -1,7 +1,9 @@
-from class_utils import PostInit
 from random import randint, choices
+from pathlib import Path
 from string import ascii_letters, digits
 from linecache import getline
+from sys import stderr
+from typing import Union
 
 
 class PassGen:
@@ -10,17 +12,32 @@ class PassGen:
 
     # static method can't be used normally
     # here because this function is monkey patched
-    def xkcd(no_of_words: int = 5, include_size: bool = True, delim: str = "-") -> str:
+    def xkcd(
+        *,
+        no_of_words: int = 5,
+        include_size: bool = True,
+        delim: str = "-",
+        word_len_range: Union[range, int] = range(4, 10),  # this can also be an int
+    ) -> str:
         word_list = []
         word_len_sum = 0
+
+        if isinstance(word_len_range, int):
+            word_pass_predicate = lambda word_len: word_len == word_len_range
+        elif isinstance(word_len_range, range):
+            word_pass_predicate = lambda word_len: word_len in word_len_range
+        else:
+            stderr.write("invalid word len qualifier!")
+
         while len(word_list) < no_of_words:
             word = getline(
-                PassGen.xkcd.WORDLIST_FILE, randint(*PassGen.xkcd.LINE_RANGE)
+                str(PassGen.xkcd.WORDLIST_FILE),
+                randint(*PassGen.xkcd.LINE_RANGE),
             ).strip()
-
-            if (word_len := len(word)) in PassGen.xkcd.WORD_LEN_RANGE:
+            if word_pass_predicate(word_len := len(word)):
                 word_len_sum += word_len
                 word_list.append(word)
+
         if include_size:
             word_list.append(
                 # take into account delimeters when doing len calulation
@@ -29,9 +46,9 @@ class PassGen:
             )
         return delim.join(word_list)
 
-    xkcd.WORDLIST_FILE = "wordlist.txt"
-    xkcd.LINE_RANGE = (0, 3000)
-    xkcd.WORD_LEN_RANGE = range(4, 10)
+    xkcd.WORDLIST_DIR = Path(__file__).parents[2]
+    xkcd.WORDLIST_FILE = (xkcd.WORDLIST_DIR / "wordlist.txt").absolute()
+    xkcd.LINE_RANGE = (4, 7700)
     xkcd = staticmethod(xkcd)
 
     @staticmethod
@@ -40,3 +57,7 @@ class PassGen:
         if with_numbers:
             domain = domain + digits
         return "".join(choices(domain, k=size))
+
+
+if __name__ == "__main__":
+    PassGen()
