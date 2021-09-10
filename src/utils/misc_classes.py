4cd4449bb7432d3fbc3cfc3879346ba47512ad9f
poison-pwn <45178461+poison-pwn@users.noreply.github.com>
2021-09-10 08:56:50 +0530

    compltely rewrote PostInit class to override class init function signature and make it the same as __post_init__

--- src/utils/misc_classes.py
+++ src/utils/misc_classes.py
@@ -1,34 +1,66 @@
-from typing import Callable
+from typing import Callable, List
 from pathlib import Path
+from functools import update_wrapper
+from inspect import signature
+from pathlib import Path
+from exceptions import PostInitNotFoundErr
 
 
-class PostInit(type):
+class PsuedoFunc(type):
+    """turns the class constructor act like a regular function and make it
+    return a value instead of an instance of the class
+
+    when the 'constructor' of the class is called i.e ClassName(args)
+    those args are passed to the __post_init__ function of the class
+    and the return value of that function is what is actually returned
+    instad of the instance
+
+    the __init__ of class also recieves the same arguments, and can still be used
+    but the instance methods of the class cannot be used from the outside as an
+    instance of the class is not returned by the constructor
+
+    the class's __init__ signature will be replaced with that of the __post_init__ function
+    to aid documetation
     """
-    enables the use of a class as a psuedofunction of sorts
 
-    it overrides the `__call__` of `type()` to actuall call an internal __post_init__
-    function of the class itself
+    def __new__(meta_cls, name, bases, dct):
+        cls_post_init = dct.get("__post_init__")
 
-    i.e when the actual class constructor is called
-
-    ```
-    value = Something() # class which has metaclass PostInit
-    #^^^^ is not an instance of the Something() class
-    ```
-
-    a class instance is NOT created, instead it returns the output of `__post_init__()`
-    of the `Something` class
-    """
-
-    def __call__(cls, *args, **kwargs):
-        try:
-            return type.__call__(cls, *args, **kwargs).__post_init__()
-        except AttributeError:
+        if cls_post_init is None:
             print(
-                f"class {cls.__name__} needs to have "
+                f"class {dct['__name__']} needs to have "
                 "__post_init__ attribute defined."
             )
-            raise
+            raise PostInitNotFoundErr(
+                "can't find __post_init__ in class passed in to PostInit"
+            )
+
+        if isinstance(cls_post_init, (staticmethod, classmethod)):
+            cls_post_init = cls_post_init.__func__  # the actual function inside
+
+        dct_init = dct.get("__init__")
+        if dct_init is None:
+            dct_init = lambda *args, **kwargs: None
+        cls_init = update_wrapper(dct_init, cls_post_init)
+
+        post_init_sig = signature(cls_post_init)
+        post_init_sig = post_init_sig.replace(
+            parameters=tuple(post_init_sig.parameters.values())
+        )
+
+        # replace the function signature of the __init__ with that of __post_init__
+        cls_init.__signature__ = post_init_sig
+
+        # replace init with the newly formed one
+        dct["__init__"] = cls_init
+        return type.__new__(meta_cls, name, bases, dct)
+
+    def __call__(cls, *args, **kwargs):
+        # make sure the __init__ runs with same args
+        instance = type.__call__(cls, *args, **kwargs)
+
+        # call the post init with the same arguments so as to imitate a real function
+        return instance.__post_init__(*args, **kwargs)
 
 
 class DirectoryTree(metaclass=PostInit):
