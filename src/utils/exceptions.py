0d380a2902af18765bf7fc2b064ffcfe5418d6f5
poison-pwn <45178461+poison-pwn@users.noreply.github.com>
2021-09-21 16:22:26 +0530

    Added Body and docstrings to Exception classes in utils.exceptions

--- src/utils/exceptions.py
+++ src/utils/exceptions.py
@@ -1,14 +1,33 @@
 class SameKeyFileError(Exception):
-    pass
+    """Error raised when public key and secret key file have the same path"""
+
+    def __init__(self, path, message):
+        self.path = path
+        self.message = message
+        super().__init__(message)
 
 
 class PostInitNotFoundErr(Exception):
+    """Error raised when PseudoFunc metaclass was attempted to use a create a class,
+    but the class did not contain __post_init__ function to run after init"""
+
+    pass
+
+
+class PassFileExistsErr(Exception):
+    """Error raised when a keyfile is attempted to be entered
+    into the keystore but it already exists"""
+
     pass
 
 
 class EmptyError(Exception):
+    """Error raised an invalid empty value is found"""
+
     pass
 
 
 class InvalidPasswordError(Exception):
+    """Error raised when password doesn't comply with criteria"""
+
     pass
