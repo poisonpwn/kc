f21fe99f522ef1aa981830dec4c8a51b3b491d7f
poison-pwn <45178461+poison-pwn@users.noreply.github.com>
2021-09-22 17:28:23 +0530

    scan requirements.txt from setup.py to install dependencies using `python setup.py install`

--- setup.py
+++ setup.py
@@ -1,9 +1,20 @@
 from setuptools import find_packages, setup
 
+with open("requirements.txt") as f:
+    install_requires = f.read().splitlines()
+
 setup(
     name="kc",
+    install_requires=install_requires,
     author="Adithyadev Rajesh",
-    extras_require={"dev": ["pytest", "pytest-ordering"]},
+    extras_require={
+        "dev": [
+            "pytest",
+            "pytest-ordering",
+            "black",
+            "flake8",
+        ]
+    },
     version="1.0",
     package_dir={"": "src"},
     packages=find_packages(where="src"),
