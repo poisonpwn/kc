0c44dfd89b2f51f073da3ab6ba538de71b7e0708
poison-pwn <45178461+poison-pwn@users.noreply.github.com>
2021-09-23 02:00:27 +0530

    fixed test_pass_store so testing password retreival from keystore occurs after password insertion

--- tests/test_pass_store.py
+++ tests/test_pass_store.py
@@ -9,15 +9,15 @@ def pass_store(tmp_path_factory) -> PassStore:
     return pass_store
 
 
-@pytest.fixture
-def inserted_credentials(pass_store: "PassStore", public_key):
-    service_name = "service_name"
-    passwd = "test_password"
+service_name = "test_service_name"
+passwd = "test_password"
+
+
+def test_insert_passwd(pass_store: "PassStore", public_key):
     pass_store.insert_passwd(service_name, passwd, public_key)
-    return service_name, passwd
 
 
-def test_retrieve_passwd(pass_store: "PassStore", inserted_credentials, secret_key):
-    service_name, passwd = inserted_credentials
+@pytest.mark.run(after="test_insert_passwd")
+def test_retrieve_passwd(pass_store: "PassStore", secret_key):
     decrypted_pass = pass_store.retrieve_passwd(service_name, secret_key)
     assert decrypted_pass == passwd
