c5262dad762243025bf959511da48e1d90603ed4
poison-pwn <45178461+poison-pwn@users.noreply.github.com>
2021-09-29 16:53:04 +0530

    removed 'pass' from empty class bodies, because a docstring is sufficient

--- src/utils/exceptions.py
+++ src/utils/exceptions.py
@@ -11,29 +11,19 @@ class PostInitNotFoundErr(Exception):
     """Error raised when PseudoFunc metaclass was attempted to use a create a class,
     but the class did not contain __post_init__ function to run after init"""
 
-    pass
-
 
 class InvalidFilenameErr(Exception):
     """Error raised when invalid filename argument is passed in"""
-
-    pass
 
 
 class PassFileExistsErr(Exception):
     """Error raised when a keyfile is attempted to be entered
     into the keystore but it already exists"""
 
-    pass
-
 
 class EmptyError(Exception):
     """Error raised an invalid empty value is found"""
 
-    pass
-
 
 class InvalidPasswordError(Exception):
     """Error raised when password doesn't comply with criteria"""
-
-    pass
