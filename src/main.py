dc8aab360220e6ca280adfb0b217639a4d18bdac
poison-pwn <45178461+poison-pwn@users.noreply.github.com>
2021-10-15 23:57:20 +0530

    started click integration by adding decorators to basic commands

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ from master_keypair import MasterKeyPair
 from pass_store import PassStore
 from utils.user_prompt import AskUser
 from pathlib import Path
+import click
 
 key_store_dir = Path.home() / ".password-store"
 master_key_pair = MasterKeyPair(
@@ -11,9 +12,11 @@ master_key_pair = MasterKeyPair(
 keystore = PassStore(key_store_dir)
 
 
-def insert_passwd(
-    service_name: str, allow_empty: bool = False, from_stdin: bool = False
-):
+@click.command()
+@click.argument("service_name", required=True)
+@click.argument("--allow-empty", is_flag=True)
+@click.argument("--stdin", "from_stdin", is_flag=True)
+def insert_passwd(service_name: str, allow_empty: bool, from_stdin: bool):
     public_key = master_key_pair.get_public_key()
     if from_stdin:
         # TODO implement piping password from stdin
@@ -24,6 +27,9 @@ def insert_passwd(
     keystore.insert_passwd(service_name, passwd, public_key)  # type: ignore
 
 
+@click.command()
+@click.argument("service_name", required=True)
+@click.option("--stdin", "master_password_from_stdin", is_flag=True)
 def retrieve_password(service_name, master_passwd_from_stdin: bool = False):
     if master_passwd_from_stdin:
         # TODO implement passing master password from stdin
@@ -33,7 +39,8 @@ def retrieve_password(service_name, master_passwd_from_stdin: bool = False):
     return keystore.retrieve_passwd(service_name, secret_key)
 
 
-def generate_keypair(from_stdin: bool = False):
+@click.command("--stdin", "from_stdin", is_flag=True)
+def generate_keypair(from_stdin: bool):
     master_passwd = None
     if from_stdin:
         # TODO implement passing password through stdin
@@ -42,10 +49,7 @@ def generate_keypair(from_stdin: bool = False):
     master_key_pair.generate_keypair(master_passwd)
 
 
+@click.command(name="list")
 def list_keystore():
     PassStore.KEY_FILE_EXT = "gpg"
     keystore.print_tree()
-
-
-if __name__ == "__main__":
-    generate_keypair()
