e2101e484f9ac792b99b2b85168e7e2c9d3870df
poison-pwn <45178461+poison-pwn@users.noreply.github.com>
2021-10-23 02:23:15 +0530

    added purgin of passfile's parent dir in test_passfile
    assert that pass_file doesn't already exist when writing a new one
    and, assert that it does, after the writing.
    
    changed name of PassFileExistsErr to PasswdFileExistsErr

--- src/utils/exceptions.py
+++ src/utils/exceptions.py
@@ -7,22 +7,21 @@ class SameKeyFileError(Exception):
         super().__init__(message)
 
 
-class PostInitNotFoundErr(Exception):
-    """Error raised when PseudoFunc metaclass was attempted to use a create a class,
-    but the class did not contain __post_init__ function to run after init"""
-
-
 class InvalidFilenameErr(Exception):
     """Error raised when invalid filename argument is passed in"""
 
 
-class PassFileExistsErr(Exception):
+class PasswdFileExistsErr(Exception):
     """Error raised when a keyfile is attempted to be entered
     into the keystore but it already exists"""
 
 
 class EmptyError(Exception):
     """Error raised an invalid empty value is found"""
+
+
+class StdinError(Exception):
+    """Error raised when password via stdin cannot be handled gracefully."""
 
 
 class InvalidPasswordError(Exception):
--- tests/test_passfile.py
+++ tests/test_passfile.py
@@ -2,11 +2,15 @@ import pytest
 from pass_store import PasswdFile
 from utils.exceptions import EmptyError
 from utils.exceptions import PasswdFileExistsErr
+from pathlib import Path
+import shutil
 
 
 @pytest.fixture(autouse=True, scope="module")
 def passfile_parent(tmp_path_factory):
-    return tmp_path_factory.mktemp("passfile_parent")
+    parent_dir: Path = tmp_path_factory.mktemp("passfile_parent")
+    yield parent_dir
+    shutil.rmtree(parent_dir)
 
 
 def test_empty_filename():
@@ -23,16 +27,19 @@ def pass_file(passfile_parent, service_name="service_name"):
 passwd = "M0Ms_-Sp46h377i"
 
 
-def test_passwd_write(pass_file: PasswdFile, public_key):
-    pass_file.write_passwd(passwd, public_key)
-    with pytest.raises(PasswdFileExistsErr):
-        pass_file.write_passwd(passwd, public_key)
-
-
 @pytest.mark.run(before="test_passwd_write")
 def test_nonexistant_read(pass_file: PasswdFile, secret_key):
+    assert not pass_file.path.exists()
     with pytest.raises(FileNotFoundError):
         pass_file.retrieve_passwd(secret_key)
+
+
+def test_passwd_write(pass_file: PasswdFile, public_key):
+    assert not pass_file.path.exists()
+    pass_file.write_passwd(passwd, public_key)
+    assert pass_file.path.exists()
+    with pytest.raises(PasswdFileExistsErr):
+        pass_file.write_passwd(passwd, public_key)
 
 
 @pytest.mark.run(after="test_passwd_write")
