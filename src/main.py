a090c8e6b441b52ce846912af82be979b4d0dea7
poison-pwn <45178461+poison-pwn@users.noreply.github.com>
2022-02-19 14:36:53 +0530

    implemented copying password to clipboard in main.retrieve_password • added alias command which aliases a passwd to another place in the   passwd_store • ctx.obj is now a KcStateObj dataclass • added '-p' and '-c' for print and copy respectively in get command • added python-deamon to requirements.txt

--- src/main.py
+++ src/main.py
@@ -1,17 +1,32 @@
+from dataclasses import dataclass
+from functools import partial
+from time import sleep
+
+import click
+
 from master_keypair import MasterKeyPair
 from pass_store import PasswdStore
+from utils.keyfiles import PublicKeyFile, SecretKeyFile
 from utils.user_prompt import AskPasswd
-from utils.keyfiles import PublicKeyFile, SecretKeyFile, PasswdFile
-import click
+from utils.misc import get_default_value_from_env
+
+
+@dataclass
+class KcStateObj:
+    master_keypair: MasterKeyPair
+    passwd_store: PasswdStore
 
 
 @click.group()
 @click.option(
     "--password-store",
     "passwd_store_path",
-    envvar=PasswdStore.PASSWD_STORE_DIR_ENV_VAR,
     type=click.Path(dir_okay=True, readable=True, writable=True),
-    default=PasswdStore.DEFAULT_LOCATION,
+    default=partial(
+        get_default_value_from_env,
+        env_var=PasswdStore.PASSWD_STORE_DIR_ENV_VAR,
+        default=PasswdStore.DEFAULT_LOCATION,
+    ),
 )
 @click.option(
     "--secret-key",
@@ -32,14 +47,14 @@ def cli(
     public_key_path,
     secret_key_path,
 ):
-    obj = {
-        "MASTER_KEYPAIR": MasterKeyPair(
+
+    ctx.obj = KcStateObj(
+        master_keypair=MasterKeyPair(
             SecretKeyFile(secret_key_path),
             PublicKeyFile(public_key_path),
         ),
-        "PASSWD_STORE": PasswdStore(passwd_store_path),
-    }
-    ctx.obj = obj
+        passwd_store=PasswdStore(passwd_store_path),
+    )
 
 
 @cli.command()
@@ -47,18 +62,19 @@ def cli(
 @click.option("--allow-empty", is_flag=True, help="allow the password to be empty")
 @click.option("-p", "--password", "passwd", required=False)
 @click.pass_obj
-def add(obj, service_name: str, allow_empty: bool, passwd):
-    public_key = obj["MASTER_KEYPAIR"].get_public_key()
+def add(obj: KcStateObj, service_name: str, allow_empty: bool, passwd: str):
+    public_key = obj.master_keypair.get_public_key()
     if passwd is None:
         passwd = AskPasswd("Enter Password: ", allow_empty=allow_empty)  # type: ignore
     elif passwd == "-":
         passwd = click.get_text_stream("stdin").readline().rstrip()
-    obj["PASSWD_STORE"].insert_passwd(service_name, passwd, public_key)  # type: ignore
+    obj.passwd_store.insert_passwd(service_name, passwd, public_key)  # type: ignore
 
 
 @cli.command(name="get")
 @click.argument("service_name", required=True)
 @click.option(
+    "-p",
     "--print/--no-print",
     "should_print",
     is_flag=True,
@@ -66,6 +82,7 @@ def add(obj, service_name: str, allow_empty: bool, passwd):
     default=False,
 )
 @click.option(
+    "-c",
     "--copy/--no-copy",
     "should_copy",
     default=True,
@@ -73,30 +90,54 @@ def add(obj, service_name: str, allow_empty: bool, passwd):
     help="copy the password to clipboard",
 )
 @click.pass_obj
-def retrieve_password(obj, service_name, should_print, should_copy):
-    get_secret_key_callback = obj["MASTER_KEYPAIR"].get_secret_key
-    passwd = obj["PASSWD_STORE"].retrieve_passwd(service_name, get_secret_key_callback)
+def retrieve_password(
+    obj: KcStateObj, service_name: str, should_print: bool, should_copy: bool
+):
+    get_secret_key_callback = obj.master_keypair.get_secret_key
+    passwd = obj.passwd_store.retrieve_passwd(service_name, get_secret_key_callback)
     if should_print:
         click.echo(passwd)
-    if should_copy:
-        pass
+    if not should_copy:
+        return
+
+    import pyperclip
+    from daemon import DaemonContext
+
+    clipboard_contents = pyperclip.paste()
+    daemon_ctx = DaemonContext(detach_process=True)
+    daemon_ctx.passwd = passwd
+    daemon_ctx.clipboard_contents = (
+        clipboard_contents if clipboard_contents is not None else ""
+    )
+    with daemon_ctx as ctx:
+        pyperclip.copy(ctx.passwd)
+        sleep(21)
+        pyperclip.copy(ctx.clipboard_contents)
 
 
 @cli.command(name="generate")
 @click.pass_obj
-def generate_keypair(obj):
-    obj["MASTER_KEYPAIR"].generate_keypair()
+def generate_keypair(obj: KcStateObj):
+    obj.master_keypair.generate_keypair()
 
 
 @cli.command()
 @click.argument("service_name")
 @click.confirmation_option(prompt="Are you sure you want to remove the password?")
 @click.pass_obj
-def remove(obj, service_name: str):
-    obj["PASSWD_STORE"].remove_passwd(service_name)
+def remove(obj: KcStateObj, service_name: str):
+    obj.passwd_store.remove_passwd(service_name)
 
 
 @cli.command(name="list")
 @click.pass_obj
-def list_keystore(obj):
-    obj["PASSWD_STORE"].print_tree()
+def list_keystore(obj: KcStateObj):
+    obj.passwd_store.print_tree()
+
+
+@cli.command("alias")
+@click.argument("source_service_name")
+@click.argument("destination_service_name")
+@click.pass_obj
+def alias(obj: KcStateObj, source_service_name: str, destination_service_name: str):
+    obj.passwd_store.alias(source_service_name, destination_service_name)
--- src/utils/misc.py
+++ src/utils/misc.py
@@ -6,3 +6,12 @@ def get_home_dir():
     if (home_dir := os.environ.get("XDG_DATA_HOME")) is not None:
         return Path(home_dir)
     return Path.home()
+
+
+def get_default_value_from_env(env_var, default, constructor=None):
+    env_var_value = os.environ.get(env_var)
+    if env_var_value is not None:
+        if constructor is not None:
+            return constructor(env_var_value)
+        return env_var_value
+    return default
